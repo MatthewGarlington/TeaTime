1f0509dcf5d937aeab5319ee9873c7da6dd43603
Matthew Garlington <SBMatthewG@gmail.com>
2021-01-21 16:50:09 -0500

    Feat: Added a detail view for Chamomile tea
    This is a detail view holds a short summary at the top of the scrollview followed by various types of Chamomile teas with various descriptions of each sub type

--- TeaTimer/TeaDetailViews/ChamomileTeaView.swift
+++ TeaTimer/TeaDetailViews/ChamomileTeaView.swift
@@ -9,7 +9,237 @@ import SwiftUI
 
 struct ChamomileTeaView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            
+            VStack(alignment: .leading) {
+                
+                Text("Chamomile Tea")
+                    .foregroundColor(.init(#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)))
+                    .bold()
+                    .font(.system(size: 40))
+                
+                
+                
+            }.padding(.top, 50)
+            
+            
+            
+            ZStack {
+                
+                Spacer()
+                    .frame(width: 420, height: 780)
+                    .background(Color.secondary)
+                    .opacity(0.8)
+                    .cornerRadius(30)
+                
+                ScrollView {
+                    
+                    VStack {
+                        
+                        Text("""
+Chamomile is a pretty, elegant, and fragrant herb that belongs in the Asteraceae plant family. (32) People have been using chamomile for therapeutic purposes for centuries, and today it’s a popular tea—especially among people who are looking to unwind before bed. Chamomile tea is made from the dried flowers of the chamomile plant.
+
+There are two primary varieties of chamomile: German Chamomile and Roman Chamomile (which is sometimes called English Chamomile). (33) They’re different species of the same plant, though they grow a bit differently and have a slightly different appearance.
+
+""" )
+                            .padding()
+                            .foregroundColor(.white)
+                        
+                        
+                        
+                        HStack {
+                            
+                            ZStack(alignment: .top) {
+                                Spacer()
+                                    .frame(width: 175, height: 330)
+                                    .background(Color.init(#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)))
+                                    .opacity(0.5)
+                                    .cornerRadius(15)
+                                    .padding()
+                                
+                                VStack {
+                                    
+                                    ZStack(alignment: .bottom) {
+                                        
+                                        Image("Chamomile2")
+                                            .resizable()
+                                            .frame(width: 175, height: 125)
+                                            .cornerRadius(15)
+                                        Spacer()
+                                            .frame(width: 175, height: 30)
+                                            .background(Color.secondary)
+                                            .cornerRadius(5)
+                                            .opacity(1.0)
+                                        
+                                        
+                                        
+                                        Text("Egyptian Chamomile")
+                                            .foregroundColor(.white)
+                                    }
+                                    ZStack {
+                                        
+                                        Spacer()
+                                            .frame(width: 175 , height: 200)
+                                            .background(Color.secondary)
+                                            .opacity(0.8)
+                                            .cornerRadius(15)
+                                        
+                                        Text("This type of chamomile is regarded as the highest quality grade of chamomile tea.")
+                                            .foregroundColor(.white)
+                                            .padding()
+                                        
+                                    }.padding()
+                                }
+                                
+                            }
+                            
+                            ZStack(alignment: .top) {
+                                Spacer()
+                                    .frame(width: 175, height: 330)
+                                    .background(Color.init(#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)))
+                                    .opacity(0.5)
+                                    .cornerRadius(15)
+                                    .padding()
+                                
+                                VStack {
+                                    
+                                    ZStack(alignment: .bottom) {
+                                        
+                                        Image("Chamomile3")
+                                            .resizable()
+                                            .frame(width: 175, height: 125)
+                                            .cornerRadius(15)
+                                        Spacer()
+                                            .frame(width: 175, height: 30)
+                                            .background(Color.secondary)
+                                            .cornerRadius(5)
+                                            .opacity(1.0)
+                                        
+                                        
+                                        
+                                        Text("German Chamomile")
+                                            .foregroundColor(.white)
+                                    }
+                                    ZStack {
+                                        
+                                        Spacer()
+                                            .frame(width: 175 , height: 200)
+                                            .background(Color.secondary)
+                                            .opacity(0.8)
+                                            .cornerRadius(15)
+                                        
+                                        Text("German chamomile offers a subtle apple flavor that is suitable for most tastes")
+                                            .foregroundColor(.white)
+                                            .padding(.horizontal)
+                                         
+                                    }.padding()
+                                }
+                                
+                            }
+                            
+                        }
+                        
+                        HStack {
+                            
+                            ZStack(alignment: .top) {
+                                Spacer()
+                                    .frame(width: 175, height: 300)
+                                    .background(Color.init(#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)))
+                                    .opacity(0.5)
+                                    .cornerRadius(15)
+                                    .padding()
+                                
+                                VStack {
+                                    
+                                    ZStack(alignment: .bottom) {
+                                        
+                                        Image("Chamomile4")
+                                            .resizable()
+                                            .frame(width: 175, height: 125)
+                                            .cornerRadius(15)
+                                        Spacer()
+                                            .frame(width: 175, height: 30)
+                                            .background(Color.secondary)
+                                            .cornerRadius(5)
+                                            .opacity(1.0)
+                                        
+                                        
+                                        
+                                        Text("Roman Chamomile")
+                                            .foregroundColor(.white)
+                                    }
+                                    ZStack {
+                                        
+                                        Spacer()
+                                            .frame(width: 175 , height: 150)
+                                            .background(Color.secondary)
+                                            .opacity(0.8)
+                                            .cornerRadius(15)
+                                        
+                                        Text("Best brands often blend Roman chamomile with sweeter herbal teas to even out the strong, bitter notes.")
+                                            .foregroundColor(.white)
+                                            .padding()
+                                        
+                                    }.padding()
+                                }
+                                
+                            }
+                            
+                            ZStack(alignment: .top) {
+                                Spacer()
+                                    .frame(width: 175, height: 300)
+                                    .background(Color.secondary)
+                                    .opacity(0.5)
+                                    .cornerRadius(15)
+                                    .padding()
+                                
+                                VStack {
+                                    
+                                    ZStack(alignment: .bottom) {
+                                        
+                                        Image("")
+                                            .resizable()
+                                            .frame(width: 175, height: 125)
+                                            .cornerRadius(15)
+                                        Spacer()
+                                            .frame(width: 175, height: 30)
+                                            .background(Color.secondary)
+                                            .cornerRadius(5)
+                                            .opacity(1.0)
+                                        
+                                        
+                                        
+                                        Text("")
+                                            .foregroundColor(.white)
+                                    }
+                                    ZStack {
+                                        
+                                        Spacer()
+                                            .frame(width: 175 , height: 150)
+                                            .background(Color.secondary)
+                                            .opacity(0.8)
+                                            .cornerRadius(15)
+                                        
+                                        Text("")
+                                            .foregroundColor(.white)
+                                            .padding()
+                                    }.padding()
+                                }
+                                
+                            }
+                        }
+                        
+                      
+                        
+                    }
+                    
+                    
+                }
+                
+            }
+        }.background(Color.black).ignoresSafeArea()
+        .navigationBarHidden(true)
     }
 }
 

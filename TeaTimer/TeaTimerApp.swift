d122c3ce5576dbc717b5fdfc0af171a236d18a17
Matthew Garlington <SBMatthewG@gmail.com>
2021-01-21 09:27:41 -0500

    Initial Commit

---
+++ TeaTimer/TeaTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeaTimerApp.swift
+//  TeaTimer
+//
+//  Created by Matthew Garlington on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct TeaTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

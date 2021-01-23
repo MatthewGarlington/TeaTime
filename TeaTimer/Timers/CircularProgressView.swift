d0f972db36e151195eab9052ed82ab506c7150cd
Matthew Garlington <SBMatthewG@gmail.com>
2021-01-23 13:48:31 -0500

    Feat: Added a circular progress bar
    This view shows the progress bar updated with the countdown time chosen to show completeion of the time and set off of the system sound alarm

--- TeaTimer/Timers/CircularProgressView.swift
+++ TeaTimer/Timers/CircularProgressView.swift
@@ -5,4 +5,45 @@
 //  Created by Matthew Garlington on 1/23/21.
 //
 
-import Foundation
+import SwiftUI
+import AVFoundation
+
+struct CircularProgressView: View {
+
+    @AppStorage("SelectedSystemSound") private var sound: SystemSounds.ID = 1005
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    @EnvironmentObject private var model: CountdownModel
+    
+    var body: some View {
+        Circle()
+            .fill(Color.clear)
+            .frame(width: size, height: size)
+            .overlay(Circle().stroke(Color(.secondarySystemBackground), lineWidth: 15))
+            .overlay(overlayCircle)
+    }
+    
+    private var overlayCircle: some View {
+        Circle()
+            .fill(Color.clear)
+            .frame(width: size, height: size)
+            .overlay(
+                Circle()
+                    .trim(from: 0, to: model.progress)
+                    .stroke(style: StrokeStyle(lineWidth: 15, lineCap: .round, lineJoin:.round))
+                    .rotationEffect(.degrees(-90))
+                    .foregroundColor(Color(model.completed(with: sound) ? .systemGreen : .systemYellow))
+                    .animation(.easeInOut(duration: 0.2))
+            )
+    }
+    
+    private var size: CGFloat {
+        horizontalSizeClass == .compact ? 300 : 540
+    }
+}
+
+struct CircularProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularProgressView()
+            .environmentObject(CountdownModel())
+    }
+}

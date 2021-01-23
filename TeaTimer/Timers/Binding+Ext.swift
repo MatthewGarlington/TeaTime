5d339c129144669978bc8be2b8f5821e129e0f0f
Matthew Garlington <SBMatthewG@gmail.com>
2021-01-23 13:40:58 -0500

    Feat: This is an added extension to the binding protocol
    Added a binding extension to keep the selected integer of minutes in the tea settings view to transtion for that value to the timer view upon selection

--- TeaTimer/Timers/Binding+Ext.swift
+++ TeaTimer/Timers/Binding+Ext.swift
@@ -5,4 +5,16 @@
 //  Created by Matthew Garlington on 1/23/21.
 //
 
-import Foundation
+import SwiftUI
+
+
+extension Binding {
+    func onChange(_ handler: @escaping (Value) -> Void) -> Binding<Value> {
+        Binding(
+            get: { self.wrappedValue },
+            set: { selection in
+                self.wrappedValue = selection
+                handler(selection)
+        })
+    }
+}

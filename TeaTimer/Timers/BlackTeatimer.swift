6ae4546019f3dc34332cd8aef19ef7337b664635
Matthew Garlington <SBMatthewG@gmail.com>
2021-01-23 14:03:20 -0500

    Feat: Added Full timer, countdown, and settings for tea timer view
    This integrates the tea timer for the countdown of the brew time and also the button to show a sheet view of the settings in order for the user to select a tea time given depending on what tea is being made

--- TeaTimer/Timers/BlackTeatimer.swift
+++ TeaTimer/Timers/BlackTeatimer.swift
@@ -7,14 +7,89 @@
 
 import SwiftUI
 
+
 struct BlackTeatimer: View {
+    @EnvironmentObject private var model: CountdownModel
+    @State private var showSettings = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                
+                ZStack {
+                    
+                    Spacer()
+                        .frame(width: 400, height: 500)
+                        .background(Color.black)
+                        .opacity(0.4)
+                        
+                        .cornerRadius(15)
+                 
+                        
+                        
+                    VStack {
+                        HStack {
+                        Spacer()
+                        Button(action: {
+                            showSettings.toggle()
+                            model.isRunning = false
+                        }) {
+                            Label("Settings", systemImage: "gearshape.fill")
+                                .font(.system(size: 25))
+                                .foregroundColor(.yellow)
+                                .labelStyle(IconOnlyLabelStyle())
+                                
+                        }
+                        .sheet(isPresented: $showSettings, onDismiss: {
+                            model.storedDuration = model.duration
+                        }) {
+                            SettingsView()
+                                .environmentObject(model)
+                        }
+                        }.padding(.horizontal)
+                        
+                        ZStack {
+                            
+                            CircularProgressView()
+                            ClockView()
+                            
+                            
+                        }
+                    }
+                  
+                    
+                    VStack {
+                      Spacer()
+                        HStack {
+                            Button(action: { model.resetTimer() }) {
+                                CircleButton(style: .reset)
+                            }
+                            Spacer()
+                            Button(action: { model.playPauseAction() }) {
+                                CircleButton(style: model.isRunning ? .pause : .start)
+                            }
+                        }.disabled(model.disabledAction())
+                        .padding()
+        
+                    }
+                }
+                
+            }.frame(width: 300, height: 400)
+            .onReceive(model.timer) { time in
+                model.receiveTimerUpdate()
+            }
+            
+            
+        }
+        
+        
     }
 }
-
+    
 struct BlackTeatimer_Previews: PreviewProvider {
     static var previews: some View {
         BlackTeatimer()
+            .environmentObject(CountdownModel())
     }
 }
+
